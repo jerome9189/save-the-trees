bcc9c9f72cc966eec45e741e8fe346d8b57ec251
thesilentsix <jerome.george.001@gmail.com>
2018-03-25 06:45:40 -0700

    Remove hard set port number

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ var mainController = require('./controllers/mainController');
 app.set('view engine', 'ejs');
 app.use(express.static('./views'));
 mainController(app);
-app.listen(8080);
-console.log('Listening to port 8080');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
