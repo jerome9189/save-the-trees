9c2ef4f42d9153b9a0ff45a4bc111a1960670954
thesilentsix <jerome.george.001@gmail.com>
2018-03-24 22:58:27 -0700

    Add dynamic insertion of values

--- views/js/result.js
+++ views/js/result.js
@@ -10,3 +10,29 @@ $(document).ready(function () {
 		}
 	});
 });
+var options = {
+  useEasing: true,
+  useGrouping: true,
+  separator: ',',
+  decimal: '.',
+};
+var demo = new CountUp('myTargetElement', 0, <%= data.carbon %>, 3, 2.5, options);
+if (!demo.error) {
+  demo.start();
+} else {
+  console.error(demo.error);
+}
+
+var demo2 = new CountUp('myTargetElement2', 0, <%= data.trees %>, 3, 2.5, options);
+if (!demo2.error) {
+  demo2.start();
+} else {
+  console.error(demo2.error);
+}
+
+var demo3 = new CountUp('myTargetElement3', 0, <%= data.water %>, 3, 2.5, options);
+if (!demo3.error) {
+  demo3.start();
+} else {
+  console.error(demo3.error);
+}
