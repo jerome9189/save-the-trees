c9cff85efe82211971ac483421c10a8f58e88011
thesilentsix <jerome.george.001@gmail.com>
2018-03-24 17:19:29 -0700

    Update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var mainController = require('./controllers/mainController');
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
+mainController(app);
 app.listen(3000);
 console.log('Listening to port 3000');
