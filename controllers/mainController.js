1b2b3bda5e11d4f3193796d076000d5e5192bf9b
thesilentsix <jerome.george.001@gmail.com>
2018-03-25 01:40:51 -0700

    Add avg stats, convert resource.html to resource.ejs

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,8 @@
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
+const precision = 3;
+var count = 0;
+var avgStats = {"trees" : 0.0, "water" : 0.0, "carbon" : 0.0}
 module.exports = function(app) {
   app.get('/index', function(req, res) {
     res.render('index');
@@ -7,16 +10,22 @@ module.exports = function(app) {
   app.post('/index',urlencodedParser, function(req, res) {
     //1 sheet = 0.002 Trees, 3 gallons water, 0.009 lb CO2
     //1 mile = 0.900lb CO2, 0,0
-    res.render('resource', {data : calculate(req.body.miles, req.body.paper, map)});
+    var stats = calculate(req.body.miles, req.body.paper, map);
+    count++;
+    avgStats = {"trees" : (parseFloat(parseFloat(avgStats.trees) + parseFloat(stats.trees))/parseFloat(count)).toFixed(precision),
+                "water" : (parseFloat(parseFloat(avgStats.water) + parseFloat(stats.water))/parseFloat(count)).toFixed(precision),
+                "carbon" : (parseFloat(parseFloat(avgStats.carbon) + parseFloat(stats.carbon))/parseFloat(count)).toFixed(precision),
+              };
+    console.log(avgStats);
+    res.render('resource', {data : stats});
     // console.log(req.body.paper);
   });
   var map = {
-    paper : {"trees" : 0.002, "water" : 3, "carbon" : 0.009},
-    miles : {"trees" : 0.0, "water" : 0, "carbon" : 0.900},
+    paper : {"trees" : 0.002, "water" : 3.0, "carbon" : 0.009},
+    miles : {"trees" : 0.0, "water" : 0.0, "carbon" : 0.900},
   }
 
   function calculate(miles, paper, map) {
-    var precision = 3;
     var netTrees = (miles * map.miles.trees) + (paper * map.paper.trees);
     var netWater = (miles * map.miles.water) + (paper * map.paper.water);
     var netCarbon = (miles * map.miles.carbon) + (paper * map.paper.carbon);
